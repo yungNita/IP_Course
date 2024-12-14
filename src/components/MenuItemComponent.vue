c02de55dc444a4321c674ad7d76506f9696cc024
yungNita <yung.nita1306@gmail.com>
2024-12-14 10:22:48 +0700

    TP4

---
+++ src/components/MenuItemComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="menuItem">
+        <div class="firstItems">
+            
+        </div>
+
+        <div class="lastItmes">
+            <div class="first">
+                <i></i>
+                <div class="second">Browse all Categories</div>
+                <i></i>
+            </div>
+
+            <div class="second"></div>
+            <div class="second">home</div>
+            <div class="second">Food</div>
+            <div class="second">Vegetables</div>
+            <div class="second">Drink</div>
+            <div class="second">Cookies</div>
+            <div class="second">Meat&SeaFood</div>
+            <div class="second">Bakery</div>
+            
+            <div class="third">
+                <i></i>
+                <div class="contact">
+                    <div class="number">1900 - 8888</div>
+                    <div class="texts">24/7 Support Center</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/components/SearchBoxComponent.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="searchBox">
+        <div class="firstItem">
+            <p class="text1">All Categories</p>
+            <p></p>
+            <p>|</p>
+            <p class="text4">Search for items</p>
+        </div>
+        <div class="lastItem">{{ u }}</div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "SearchBoxComponent",
+    props: {
+
+    }
+}
+</script>
+
+<style scoped>
+.searchBox{
+    display: flex;
+    justify-content: space-between;
+    border: 2px solid #BCE3C9;
+    border-radius: 5px;
+    width: 700px;
+    height: 50px;
+}
+
+.firstItem{
+    display: flex;
+}
+
+.text1{
+    color: #253D4E;
+    font-size: 12px;
+    line-height: 20px;
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+i{
+    width: 11px;
+    height: 6px;
+    color: #7E7E7E;
+}
+
+.text4{
+    line-height: 18px;
+    font-size: 12px;
+    font-weight: 400px;
+    
+}
+</style>
